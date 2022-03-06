8d5aa267605b0d303712e588a6b986d0c1adc6bb
Luke Donghyun Lee <lukeleeai@Lukes-MacBook-Pro.local>
2022-03-06 23:48:16 +0000

    back to origin

--- metnet/layers/ConditionTime.py
+++ metnet/layers/ConditionTime.py
@@ -4,7 +4,6 @@ from torch import nn as nn
 
 def condition_time(x, i=0, size=(12, 16), seq_len=15):
     "create one hot encoded time image-layers, i in [1, seq_len]"
-    print(x.device)
     assert i < seq_len
     times = (torch.eye(seq_len, dtype=x.dtype, device=x.device)[i]).unsqueeze(-1).unsqueeze(-1)
     ones = torch.ones(1, *size, dtype=x.dtype, device=x.device)
